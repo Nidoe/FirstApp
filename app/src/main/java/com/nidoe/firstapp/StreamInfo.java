3a23f7b2df3c68d6ccc33f6b8c48094fcc3f64d9
Luv Somani <lrsfh3@mail.missouri.edu>
2015-05-05 19:28:08 -0700

    123

--- app/src/main/java/com/nidoe/firstapp/StreamInfo.java
+++ app/src/main/java/com/nidoe/firstapp/StreamInfo.java
@@ -1,15 +1,33 @@
 package com.nidoe.firstapp;
 
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
 
 
 public class StreamInfo extends ActionBarActivity {
-
+    TextView txtView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,11 +35,16 @@ public class StreamInfo extends ActionBarActivity {
 
         Intent intent = getIntent();
         String name = intent.getStringExtra("Display Name");
-
-        TextView view = (TextView)findViewById(R.id.streamname);
-
-        view.setText(name);
-
+        String origUrl = "https://api.twitch.tv/kraken/streams?channel=" + name;
+        txtView = (TextView)findViewById(R.id.streamname);
+        ConnectivityManager connMgr = (ConnectivityManager)
+                getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+        if (networkInfo != null && networkInfo.isConnected()) {
+            new DownloadWebpageTask().execute(origUrl);
+        } else {
+            txtView.setText("No network connection available.");
+        }
     }
 
     @Override
@@ -44,5 +67,74 @@ public class StreamInfo extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+    private class DownloadWebpageTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... urls) {
+
+            // params comes from the execute() call: params[0] is the url.
+            try {
+                String result = downloadUrl(urls[0]);
+                return result;
+            } catch (Exception e) {
+                return "Unable to download URL properly";
+
+            }
+        }
+        // onPostExecute displays the results of the AsyncTask.
+        @Override
+        protected void onPostExecute(String result) {
+            try {
+                JSONObject jsonObj = new JSONObject(result);
+                JSONArray stream = jsonObj.getJSONArray("streams");
+                JSONObject streamDetails = stream.getJSONObject(0);
+                txtView.setText("Game: " + streamDetails.getString("game") + "\nViewers: " + streamDetails.get("viewers"));
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private String downloadUrl(String myurl) throws IOException {
+        InputStream is = null;
+        // Only display the first 500 characters of the retrieved
+        // web page content.
+
+        try {
+            URL url = new URL(myurl);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setReadTimeout(10000 /* milliseconds */);
+            conn.setConnectTimeout(15000 /* milliseconds */);
+            conn.setRequestMethod("GET");
+            conn.setDoInput(true);
+            // Starts the query
+            conn.connect();
+            int response = conn.getResponseCode();
+
+            is = conn.getInputStream();
+
+            // Convert the InputStream into a string
+            String contentAsString = readIt(is);
+            is.close();
+            return contentAsString;
+
+            // Makes sure that the InputStream is closed after the app is
+            // finished using it.
+        } finally {
+            if (is != null) {
+                is.close();
+            }
+        }
+    }
+
+    public String readIt(InputStream stream) throws IOException, UnsupportedEncodingException {
+        BufferedReader bufferedReader = new BufferedReader( new InputStreamReader(stream));
+        String line = "";
+        String result = "";
+        while((line = bufferedReader.readLine()) != null)
+            result += line;
+
+        return result;
     }
 }
